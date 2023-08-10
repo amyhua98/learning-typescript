a103ec5877eccaaa92ce7482e96f21057b376c8a
Amy Hua <aymay98@gmail.com>
2023-08-10 15:00:33 -0500

    New folder

---
+++ basisAndBasisTypes/basics.ts
@@ -0,0 +1,18 @@
+function add(n1: number, n2: number, showResults: boolean, phrase: string){
+    if (typeof n1 !== 'number' ||  typeof n2 !== 'number' ){
+        throw new Error('Incorrect input!')
+    }
+    const result = n1 + n2
+    if (showResults){
+        console.log(phrase + result)
+    } else{
+        return n1 + n2
+    }
+}
+
+const number1 = 5
+const number2 = 2.8
+const printResults = true
+const resultPhrase = "Results is: "
+
+add(number1, number2, printResults, resultPhrase)
