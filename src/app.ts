fc72eca7e256eec9fc0dfe4ca54e55e917657e8d
Amy Hua <aymay98@gmail.com>
2024-01-05 16:49:33 -0600

    Adding generic work

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,27 @@
  */
 
 
-const names = ['kiki', 'koko']
+// const names = ['kiki', 'koko']
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve ('This is done');
+//     }, 2000),
+// });
+
+// promise.then(data => {
+//     data.split(' ')
+// })
+
+
+//T and U is essential saying theres gonna be a that is type Object blah and b that is type Object blah blah
+//good in situation when we have a vague variable and better readability
+function merge<T, U>(a: T, b: U) {
+    return Object.assign(a,b);
+}
+
+// the T would represent something like this example
+// const mergedObj = merge({name: 'Max'}, {age: 30}) as {name: string, age: number}
+
+const mergedObj = merge({name: 'Max'}, {age: 30}) as {name: string, age: number}
+console.log(mergedObj.age)
