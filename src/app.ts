98135457f5dddb682cbd79ca5c173a402d2d2317
Amy Hua <aymay98@gmail.com>
2024-02-20 14:22:01 -0600

    Add decortor work

--- src/app.ts
+++ src/app.ts
@@ -21,14 +21,21 @@ function Logger(logString: string){
 // the Wtih template is the factory, the function statement inside the WithTemplate is the actual decorator
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE LOGGER');
-    return function( constructor: any) {
-        console.log('Render template')
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl){
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name
-        }
+    //new(...args: any[]) : {} - get in some object of any type that is new 
+    return function<T extends {new(...args: any[]) : {name: string}}>( originalConstructor: T) {
+        //syntatic sugar - syntax or language construct that doesn't introduce new functionality but makes the code more convenient or expressive
+        // returns a new constructor function replaces orginial class and constructor
+        return class extends originalConstructor {
+            constructor(..._: any[]){
+                super();
+                console.log('Render template')
+                const hookEl = document.getElementById(hookId);
+                if (hookEl){
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name
+                }
+            }
+        };
     }
 }
 
