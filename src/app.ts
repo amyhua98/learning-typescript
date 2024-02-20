14f97a51e99a2d8ebfc9f167391a5307e9ba1b9d
Amy Hua <aymay98@gmail.com>
2024-02-20 14:50:54 -0600

    Add validation with decorator

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,7 @@ function WithTemplate(template: string, hookId: string) {
     return function<T extends {new(...args: any[]) : {name: string}}>( originalConstructor: T) {
         //syntatic sugar - syntax or language construct that doesn't introduce new functionality but makes the code more convenient or expressive
         // returns a new constructor function replaces orginial class and constructor
+        // the _ means we dont really care about it
         return class extends originalConstructor {
             constructor(..._: any[]){
                 super();
@@ -112,5 +113,117 @@ class Product {
     /**
      * All decorators when classes are defined
      */
-
 }
+
+const p1 = new Product('Book', 29);
+const p2 = new Product('Book 2', 29);
+
+//-- "Autobind" Decorator
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalConstructor = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get(){
+            const boundFn = originalConstructor.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works';
+
+    @AutoBind
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+// as of now, it is listening to addEventListener and not connecting the p.showMessage to the p that is the newPrinter has to use .bind() to connect them
+// const button = document.querySelector('button')!;
+// button.addEventListener('click', p.showMessage.bind(p));
+
+// instead can use decorator to bind the p
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
+
+
+// -- Validation with Decorator
+
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // e.g. ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  };
+}
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+
+courseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert('Invalid input, please try again!');
+    return;
+  }
+  console.log(createdCourse);
+});
